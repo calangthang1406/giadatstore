e7fec3d87fc076d919ffdbf54c36734b163c9a0d
calangthang1406 <nguyentrananhkhoa.wf@gmail.com>
2022-08-25 00:18:11 +0700

    create admin site

--- home_store/models.py
+++ home_store/models.py
@@ -3,6 +3,10 @@ from turtle import title
 from django.db import models
 
 class Post(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=100)
     body = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.title
+    
