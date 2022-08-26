9bc65f5a9d6aa90dcfa142def6d19db3366efd35
Nedsion <quydu23122@gmail.com>
2022-08-26 15:26:36 +0700

    change locations save media

--- post/models.py
+++ post/models.py
@@ -4,7 +4,9 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=100)
     body = models.TextField()
-    image = models.ImageField()
+    image =  models.ImageField(
+        upload_to="media/",
+    )
     price = models.CharField(max_length=50)
     date = models.DateTimeField(auto_now_add=True)
     
